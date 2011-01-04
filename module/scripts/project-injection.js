6911ffb7cfce672b4fd889d5565f391dddd9e9b0
maxogden <max@maxogden.com>
2011-01-04 13:47:53 -0800

    basic modal dialog for uploader

--- module/scripts/project-injection.js
+++ module/scripts/project-injection.js
@@ -1,16 +1,28 @@
 var upload = function() { 
-  body = {};
-  updateOptions = {};
-
   Refine.postProcess(
       "uploader",
       "uploader",
-      {},
-      body,
-      updateOptions,
-      {}
+      {},{},{},
+      { 
+          "onDone": function(response) {
+              console.log(response);
+          }
+      }
   );
 }
+
+var showUploaderDialog = function() {
+  var dialog = $(DOM.loadHTML("uploader", "scripts/uploader-dialog.html"));
+
+  var elmts = DOM.bind(dialog);
+  elmts.dialogHeader.text("Data Uploader");
+
+  var level = DialogSystem.showDialog(dialog);
+
+  elmts.okButton.click(function() {
+      DialogSystem.dismissUntil(level - 1);
+  }); 
+};
 
 MenuSystem.insertAfter(
     ExporterManager.MenuItems,
@@ -18,7 +30,7 @@ MenuSystem.insertAfter(
     {
       "id": "uploader/uploader",
       "label": "Upload...",
-      "click": upload
+      "click": showUploaderDialog
     }
 );
 
